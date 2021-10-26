46435b9233a3309bfbb1bdc60ab923c2071065ba
NFTylerjet <NFTylerjet@gmail.com>
2021-10-25 22:24:20 -0700

    update date format

--- express.js
+++ express.js
@@ -20,7 +20,7 @@ const getFileUpdatedDate = (path) => {
 }
 function modifiedDate() {
   try {
-    lastupdated = date.format(getFileUpdatedDate('./public/files/CombinedPassHolders.csv'),'MMM DDD YYYY hh:mm:ss');
+    lastupdated = date.format(getFileUpdatedDate('./public/files/CombinedPassHolders.csv'),'MMM DDD YYYY hh:mm:ss A', true);
   } catch (error) {
     console.log(error.message);
   }
